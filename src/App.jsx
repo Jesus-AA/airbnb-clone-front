6c092022999ba0ca0b3552c9cc33a4b54dffee23
Jesus Alvarez <jesusalvarezaguilar94@gmail.com>
2023-12-11 12:04:46 +0100

    Add register form

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import IndexPage from './components/index-page/index-page';
 import Layout from './components/layout/layout';
 import LoginPage from './components/login-page/login-page';
+import RegisterPage from './components/register/register';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Route path="/" element={<Layout />}>
         <Route index element={<IndexPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
       </Route>
     </Routes>
   );
