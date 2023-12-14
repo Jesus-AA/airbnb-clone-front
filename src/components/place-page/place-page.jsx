f0bdbbd2b7bcf10d9bb801dec0965a43cf6acd41
Jesus Alvarez <jesusalvarezaguilar94@gmail.com>
2023-12-14 20:57:59 +0100

    Add extra info to place details

--- src/components/place-page/place-page.jsx
+++ src/components/place-page/place-page.jsx
@@ -58,7 +58,7 @@ export function PlacePage() {
   }
 
   return (
-    <div className="mt-4 bg-gray-100 -mx-8 px-8 py-8">
+    <div className="mt-4 bg-gray-100 -mx-8 px-8 pt-8">
       <h1 className="text-3xl">{place.title}</h1>
       <a
         className="flex gap-1 my-3 font-semibold underline"
@@ -85,7 +85,6 @@ export function PlacePage() {
             d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z"
           />
         </svg>
-
         {place.address}
       </a>
       <div className="relative">
@@ -139,7 +138,7 @@ export function PlacePage() {
         </button>
       </div>
 
-      <div className="mt-8 grid gap-8 grid-cols-1 md:grid-cols-[]2fr_1fr">
+      <div className="mt-8 mb-8 grid gap-8 grid-cols-1 md:grid-cols-[]2fr_1fr">
         <div>
           <div className="my-4">
             <h2 className="font-semibold text-2xl">Description</h2>
@@ -151,6 +150,14 @@ export function PlacePage() {
         </div>
         <div>
           <BookingWidget place={place} />
+        </div>
+      </div>
+      <div className="bg-white -mx-8 px-8 py-8 border-t">
+        <div>
+          <h2 className="font-semibold text-2xl">Extra info:</h2>
+        </div>
+        <div className="mb-4 mt-1 text-sm text-gray-700 leading-4">
+          {place.extraInfo}
         </div>
       </div>
     </div>
