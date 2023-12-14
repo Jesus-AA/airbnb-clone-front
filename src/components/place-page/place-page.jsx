ed4d90c494d933a8de6dca71ca3f02871cb50a37
Jesus Alvarez <jesusalvarezaguilar94@gmail.com>
2023-12-14 20:33:18 +0100

    Add booking form

--- src/components/place-page/place-page.jsx
+++ src/components/place-page/place-page.jsx
@@ -60,15 +60,35 @@ export function PlacePage() {
     <div className="mt-4 bg-gray-100 -mx-8 px-8 py-8">
       <h1 className="text-3xl">{place.title}</h1>
       <a
-        className="my-2 block font-semibold underline"
+        className="flex gap-1 my-3 font-semibold underline"
         target="_blank"
         href={'https://maps.google.com/?q=' + place.address}
         rel="noreferrer"
       >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z"
+          />
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z"
+          />
+        </svg>
+
         {place.address}
       </a>
       <div className="relative">
-        <div className="grid gap-2 grid-cols-[2fr_1fr]">
+        <div className="grid gap-2 grid-cols-[2fr_1fr] rounded-3xl overflow-hidden">
           <div>
             {place.photos?.[0] && (
               <div>
@@ -116,6 +136,44 @@ export function PlacePage() {
           </svg>
           Show more photos
         </button>
+      </div>
+      <div className="my-4">
+        <h2 className="font-semibold text-2xl">Description</h2>
+        {place.description}
+      </div>
+      <div className="grid grid-cols-2">
+        <div>
+          Check-in: {place.checkIn} <br />
+          Check-out: {place.checkOut} <br />
+          Max number of guests: {place.maxGuests}
+        </div>
+        <div>
+          <div className="bg-white shadow py-4 rounded-2xl">
+            <div className="text-2xl text-center">
+              Price: ${place.price} / night
+            </div>
+            <div className="border rounded-2xl mt-4">
+              <div className="flex">
+                <div className="py-3 px-4">
+                  <label htmlFor="">Check in:</label>
+                  <input type="date" />
+                </div>
+                <div className="py-3 px-4 border-l">
+                  <label htmlFor="">Check out:</label>
+                  <input type="date" />
+                </div>
+              </div>
+              <div>
+                <div className="py-3 px-4 border-t">
+                  <label htmlFor="">Number of guests:</label>
+                  <input type="number" />
+                </div>
+              </div>
+            </div>
+
+            <button className="primary mt-4">Book this place</button>
+          </div>
+        </div>
       </div>
     </div>
   );
